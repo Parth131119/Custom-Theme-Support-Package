476e2b03e3a4af92d5b792f368933cc580887570
Parth131119 <122441677+Parth131119@users.noreply.github.com>
2023-11-21 14:00:00 +0530

    Update header.php

--- custom theme parent child pack/custommadetheme/header.php
+++ custom theme parent child pack/custommadetheme/header.php
@@ -5,15 +5,13 @@
 $custom_options = get_option('custom_theme_options');
 ?>
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="width=device-width">
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 <title>
 <?php wp_title('|', true, 'right'); ?>
 </title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 <?php if (!empty($custom_options['favicon'])) { ?>
 <link rel="shortcut icon" href="<?php echo esc_url($custom_options['favicon']); ?>">
 <?php } ?>
